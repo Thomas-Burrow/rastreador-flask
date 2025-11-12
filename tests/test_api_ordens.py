9486982b5c8d09ae9237bfd32f0543b64239e850
Thomas Patrick Burrow <thomas.burrow@acad.ufsm.br>
2025-11-12 15:05:08 -0300

    Adiciona mais um teste

--- tests/test_api_ordens.py
+++ tests/test_api_ordens.py
@@ -10,6 +10,12 @@ def test_ordens_por_placa_2(client):
     assert b'null' in response.data
     assert b"XYZ12C4" in response.data
 
+def test_ordens_por_placa_caseinsentivo(client):
+    response = client.get('/api/ordens/por_placa/xyz12C4')
+    assert response.status_code == 200
+    assert b'null' in response.data
+    assert b"XYZ12C4" in response.data
+
 def test_ordens_por_placa_ruim(client):
     response = client.get('/api/ordens/por_placa/ZZZ-9999')
     assert response.status_code == 404
